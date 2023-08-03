d486b2e2204153f074880f14be6706357f91833c
Nico De Witte <bioboost@gmail.com>
2023-08-03 08:40:24 +0200

    Setup four switch node that is configurable and supports nucleo l476rg and lpc1768

--- README.md
+++ README.md
@@ -15,3 +15,19 @@ This firmware is part of the Bio Home Automation System - BHAS (pronounced BeeHa
 
 * [bhas](https://github.com/BioBoost/mbed-bhas): The BHAS library
 * [commander](https://github.com/BioBoost/mbed-commander): Serial commander library (special thanks @ [sillevl](https://github.com/sillevl))
+
+## Device Configuration
+
+Connect the device via the serial port with a baudrate of `115200` and double tap the reset button to get it into command mode.
+
+In command mode the following commands are supported:
+
+| Command | Description |
+| --- | --- |
+| `VERSION` | Get the firmware version and build timestamp |
+| `NODEID` | Get the current Node ID |
+| `NODEID=xx` | Set the Node ID |
+| `GWID` | Get the current Gateway ID |
+| `GWID=xx` | Set the Gateway ID |
+| `RESET!` | Reboot the device |
+| `FACTORYRESET!` | Reset the device to its factory defaults |
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,75 @@
 #include "mbed_events.h"
 #include "mbed.h"
 #include "four_switch_node.h"
+#include "relay_node.h"
 #include "local_echo_channel.h"
+#include "version.h"
+#include "mbed_trace.h"
+#include "configuration_manager.h"
+#include "command_manager.h"
+
+#if defined(TARGET_NUCLEO_L476RG) || defined(TARGET_LPC1768)
+#include "can_channel.h"
+using BHAS::Communication::Channels::CANChannel;
+#endif
+
+#define TRACE_GROUP "BHAS Node main"
 
 using BHAS::Nodes::FourSwitchNode;
+using BHAS::Nodes::RelayNode;
 using BHAS::Communication::Channels::LocalEchoChannel;
-
-// using BHAS::Communication::Channels::CANChannel;
-// CAN _canBus(D15, D14);
-// CANChannel _canChannel(_canBus);
-
-LocalEchoChannel localEchoChannel;
-FourSwitchNode node(88, 1, localEchoChannel);       // TODO: Can we use sille's command thingy to configure for example this ID and gateway ID
+using BHAS::ConfigurationManager;
+using BHAS::CommandManager;
 
 int main() {
-  ThisThread::sleep_for(2s);
-  printf("... Booting BHAS node firmware ...\r\n");
-  ThisThread::sleep_for(2s);
+  mbed_trace_init();
 
-  node.take_over();
+  tr_info("Booting BHAS node firmware");
+  tr_info("Firmware version: %s", BHAS_FIRMWARE_VERSION);
+  tr_info("Target: %s", MBED_STRINGIFY(TARGET_NAME));
+
+#ifdef MBED_CONF_RTOS_PRESENT
+  const char* profile = "rtos";
+#else
+  const char* profile = "bare-metal";
+#endif
+  tr_info("Mbed OS version: %d.%d.%d (profile: %s)", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION, profile);
+    
+  ConfigurationManager config;
+  config.init();
+  tr_info("This device has booted %lu times", config.boot_count());
+
+  bool startCommandManager = config.double_reset();
+  if (startCommandManager) tr_info("Detected double reset");
+
+  config.set_double_reset();
+  ThisThread::sleep_for(2s);    // Basically if we reset within this time window, command manager is started
+  config.clear_double_reset();
+
+  if(startCommandManager) {
+    LowPowerTimer consoleTimeout;
+    consoleTimeout.start();
+    CommandManager commandManager(config);
+    commandManager.on_command([&consoleTimeout] () { consoleTimeout.reset(); });
+
+    while(consoleTimeout.elapsed_time() < std::chrono::seconds(MBED_CONF_APP_COMMAND_CONSOLE_TIMEOUT)) {
+      commandManager.dispatch();
+    }
+  }
+
+#if defined(TARGET_NUCLEO_L476RG)
+  CAN canBus(D15, D14);
+  CANChannel channel(canBus);
+#elif defined(TARGET_LPC1768)
+  CAN canBus(p30, p29);
+  CANChannel channel(canBus);
+#else
+  LocalEchoChannel channel;
+#endif
+
+  FourSwitchNode node(config.node_id(), config.gateway_id(), channel);
+  // RelayNode node(config.node_id(), config.gateway_id(), localEchoChannel);
+
+  tr_info("Passing control to node");
+  node.dispatch_forever();
 }
